9dc90ef7cdcb6859355e3204862b5e370ff51edd
Adrián Sigüenza Ramírez <adrisira07@gmail.com>
2023-06-22 17:27:40 +0200

    1.2

---
+++ C.Programming.Lenguage/Chapter1/fahrenheitToCelcius.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*print Fahrenheit-Celcius table
+    for fahr = 0, 20, ..., 300 */
+
+/*El int antes del main no viene en el ejemplo, pero te salta un warning si no lo pones, 
+simplemente le dices que la funcion main va a devolver integers*/
+float main()
+{
+    float fahr, celcius;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper){
+        celcius = (5.0/9.0) * (fahr-32.0);
+        printf("%3.0f\t%6.1f\n", fahr, celcius);
+        fahr = fahr + step;
+    }
+}
